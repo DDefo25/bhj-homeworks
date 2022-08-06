1d18c91903f0dbee261fe7db0a652f3b8a1b71cb
ddefo25 <ispumamm@gmail.com>
2022-08-06 14:12:44 +0200

    task 2 is completed

---
+++ element-search/menu/task.js
@@ -0,0 +1,19 @@
+const menuLinkEl = document.getElementsByClassName('menu__link');
+ 
+Array.from(menuLinkEl, el => {
+    el.onclick = () => {
+        let menuSubEl = el.closest('.menu__item').querySelector('.menu_sub');
+        let menuActiveEl = document.querySelector('.menu_main .menu_active');
+
+        if (menuSubEl.matches('.menu_active')) {
+            menuSubEl.classList.remove('menu_active');
+        } else {
+            if (menuActiveEl !== null) {
+                menuActiveEl.classList.remove('menu_active');
+            }
+            menuSubEl.classList.add('menu_active');
+        }
+  
+        return false
+    };
+})  
