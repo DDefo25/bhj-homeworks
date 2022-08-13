95115151aecd57e49d561b078916827070048099
ddefo25 <ispumamm@gmail.com>
2022-08-13 10:48:23 +0200

    change navigation from parentElement.previousElementSibling to .closest.querySelector

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -7,22 +7,10 @@ Array.from(dropdownValueEl, el => {
     }
 });
 
-// Array.from(dropdownItemEl, el => {
-//     el.onclick = event => {
-//         console.time('1')
-//         event.currentTarget.closest('.dropdown').querySelector('.dropdown__value').textContent = event.target.textContent;
-//         event.currentTarget.parentElement.classList.remove('dropdown__list_active');
-//         console.timeEnd('1')
-//         return false;
-//     }
-// })
-
 Array.from(dropdownItemEl, el => {
     el.onclick = event => {
-        console.time('1')
-        event.currentTarget.parentElement.previousElementSibling.textContent = event.target.textContent;
+        event.currentTarget.closest('.dropdown').querySelector('.dropdown__value').textContent = event.target.textContent;
         event.currentTarget.parentElement.classList.remove('dropdown__list_active');
-        console.timeEnd('1')
         return false;
     }
 })
