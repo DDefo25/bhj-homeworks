969dfcbf287c6194f0930608938e356b1f227e2a
ddefo25 <ispumamm@gmail.com>
2022-09-03 15:57:19 +0200

    book-reader+ is completed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,27 +1,63 @@
-const fontSizeEl = document.querySelectorAll('.font-size');
+const controlFontSizeEl = document.querySelectorAll('.font-size');
+const controlColorTextEl = document.querySelectorAll('.book__control_color > .color');
+const controlColorBgEl = document.querySelectorAll('.book__control_background > .color');
 
-fontSizeEl.forEach(el => {
-    el.addEventListener('click', event => {
+controlFontSizeEl.forEach(el => {
+    el.onclick = () => {
         changeFontSize(el);
-        event.preventDefault();
-    })
+        return false;
+    }
+})
+
+controlColorTextEl.forEach(el => {
+    el.onclick = () => {
+        changeColorText(el, controlColorTextEl);
+        return false;
+    }
+})
+
+controlColorBgEl.forEach(el => {
+    el.onclick = () => {
+        changeColorText(el, controlColorBgEl);
+        return false;
+    }
 })
 
 function changeFontSize(el) {
-    [...fontSizeEl].find(el => el.classList.toggle('font-size_active', false));
-    el.classList.add('font-size_active');
+    [...controlFontSizeEl].find(el => el.classList.toggle('font-size_active', false));
+    el.classList.add('font-size_active');   
 
     const bookEl = el.closest('.book');
 
-    if (bookEl.classList.contains('book_fs-big')) {
-        bookEl.classList.remove('book_fs-big');
-    } else if (bookEl.classList.contains('book_fs-small')) {
-        bookEl.classList.remove('book_fs-small');
+    const bookElClass = [...bookEl.classList].find(cl => /^book_fs-/.test(cl));
+
+    if (bookElClass !== undefined) {
+        bookEl.classList.remove(bookElClass);
     }
 
-    if (el.classList.contains('font-size_big')) {
-        bookEl.classList.add('book_fs-big');
-    } else if (el.classList.contains('font-size_small')) {
-        bookEl.classList.add('book_fs-small');        
+    bookEl.classList.add(`book_fs-${el.dataset.size}`);
+};
+
+function changeColorText(el, arr) {
+    [...arr].find(el => el.classList.toggle(`color_active`, false));
+    el.classList.add(`color_active`);
+
+    const bookControl = el.closest('.book__control');
+    let targetDataSet;
+
+    if (bookControl.classList.contains('book__control_color')) {
+        targetDataSet = ['color', 'textColor'];
+    } else if (bookControl.classList.contains('book__control_background')) {
+        targetDataSet = ['bg', 'bgColor'];
     }
+
+    const bookEl = el.closest('.book');
+    const re = new RegExp(`^book_${targetDataSet[0]}`);
+    const bookElClass = [...bookEl.classList].find(class_ => re.test(class_));
+
+    if (bookElClass !== undefined) {
+        bookEl.classList.remove(bookElClass);
+    }
+
+    bookEl.classList.add(`book_${targetDataSet[0]}-${el.dataset[targetDataSet[1]]}`);
 };
